7951231e41c8b3a03648db06a2fa8bea03d22fb3
James Brown <bjames1@vt.edu>
2020-01-20 18:02:49 -0500

    Add files via upload

--- make_container.py
+++ make_container.py
@@ -2,6 +2,6 @@ def make_container(keyList, itemList):
     container = {};
     for j in range(len(itemList)):
         key = keyList[j]
-        item = {key: { i : i for i in itemList}}
+        item = {key: { i : None for i in itemList}}
         container.update(item)
     return container
